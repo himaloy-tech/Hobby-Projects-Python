beef27c3a3659ebdd77a350ad309217da522b371
himaloy-tech-beep <72344109+himaloy-tech-beep@users.noreply.github.com>
2020-10-08 08:57:16 +0530

    Added a notification

--- pdf text extracter.py
+++ pdf text extracter.py
@@ -1,9 +1,15 @@
 import PyPDF2
 from PackageHimaloy import *
-
+from plyer import notification
 user_input = int(input("Page number: "))
 user_input = user_input - 1
 reader = PyPDF2.PdfFileReader('Files/Biology.pdf')
 text = reader.getPage(user_input).extractText()
 createfile('Extracted text', text)
-print("Done")
+if __name__ == '__main__':
+  notification.notify(
+    title= f"A File has been created",
+    message="A File Named Extracted text has been created CHECK YOUR DESKTOP!!",
+    app_icon="glassofwater.ico",
+    timeout=10
+  )
