8b8336c42222bfba0c6100c5c1b96189d7b99386
Himaloy <himaloymondal100@gmail.com>
2020-12-19 11:08:10 +0530

    Extract text without asking page number

--- Other/pdf text extracter.py
+++ Other/pdf text extracter.py
@@ -2,14 +2,15 @@ import PyPDF2
 from PackageHimaloy import *
 from plyer import notification
 
-user_input = int(input("Page number: "))
-user_input = user_input - 1
 reader = PyPDF2.PdfFileReader('Files/Biology.pdf')
-text = reader.getPage(user_input).extractText()
+text = ""
+for page_number in range(reader.getNumPages()):
+    text += reader.getPage(page_number).extractText()
 createfile('Extracted text', text)
+
 if __name__ == '__main__':
     notification.notify(
-        title=f"A File has been created",
+        title="A File has been created",
         message="A File Named Extracted text has been created CHECK YOUR DESKTOP!!",
         app_icon="images/glassofwater.ico",
         timeout=10
